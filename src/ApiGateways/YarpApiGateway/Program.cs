e4d94180129216b44ddbef993e12355d73a9b0f8
sachin kumar <sachindewan12@gmail.com>
2024-06-02 17:34:04 +0530

    implemented ordering microservice and updated docker compose file

---
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+//register yarp service
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+var app = builder.Build();
+
+
+app.Run();
